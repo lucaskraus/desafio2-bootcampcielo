e71d682bdafe41f857ffc763ac0ec5f143c6d8ed
Lucas Kraus <lucas-kraus@hotmail.com>
2023-09-25 21:41:07 -0300

    UPDATE - Criação das entidades e mais dependências.
    Criação das duas entidades principais da aplicação: PessoaFisica e PessoaJuridica.
    
    Também, foram adicionadas as dependências necessárias para utilizar as beans apropriadas para as entidades.

---
+++ server/src/main/java/application/web/bootcampcielo/domain/PessoaFisica.java
@@ -0,0 +1,22 @@
+package application.web.bootcampcielo.domain;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.*;
+
+@Entity
+public class PessoaFisica {
+    @Id
+    @Size(min = 11, max = 11, message = "O CPF deve ter 11 dígitos")
+    @Pattern(regexp = "^\\d{11}$", message = "O CPF deve conter apenas números")
+    private String cpf;
+
+    @Size(max = 4, message = "O MCC deve ter no máximo 4 caracteres")
+    private String mcc;
+
+    @NotBlank(message = "O nome não pode estar em branco")
+    @Size(max = 50, message = "O nome deve ter no máximo 50 caracteres")
+    private String nome;
+
+    @Pattern(regexp = "^[a-zA-Z0-9_\\-\\.]+@[a-zA-Z0-9_\\-\\.]+\\.[a-zA-Z]{2,5}$", message = "O email não é válido")
+    private String email;
+}
---
+++ server/src/main/java/application/web/bootcampcielo/domain/PessoaJuridica.java
@@ -0,0 +1,30 @@
+package application.web.bootcampcielo.domain;
+import jakarta.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.*;
+
+@Entity
+public class PessoaJuridica {
+    @Id
+    @Size(min = 14, max = 14, message = "O CNPJ deve ter 14 dígitos")
+    @Pattern(regexp = "^\\d{14}$", message = "O CNPJ deve conter apenas números")
+    private String cnpj;
+
+    @NotBlank(message = "A razão social não pode estar em branco")
+    @Size(max = 50, message = "A razão social deve ter no máximo 50 caracteres")
+    private String razaoSocial;
+
+    @Size(max = 4, message = "O MCC deve ter no máximo 4 caracteres")
+    private String mcc;
+
+    @Size(min = 11, max = 11, message = "O CPF do contato deve ter 11 dígitos")
+    @Pattern(regexp = "^\\d{11}$", message = "O CPF do contato deve conter apenas números")
+    private String cpfContato;
+
+    @NotBlank(message = "O nome do contato não pode estar em branco")
+    @Size(max = 50, message = "O nome do contato deve ter no máximo 50 caracteres")
+    private String nomeContato;
+
+    @Pattern(regexp = "^[a-zA-Z0-9_\\-\\.]+@[a-zA-Z0-9_\\-\\.]+\\.[a-zA-Z]{2,5}$", message = "O email do contato não é válido")
+    private String emailContato;
+}
