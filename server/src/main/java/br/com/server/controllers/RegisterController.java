22a1147d454af97ef4d5343f1b2b5f4b12291d18
Lucas Kraus <lucas-kraus@hotmail.com>
2023-09-26 21:05:48 -0300

    UPDATE - Implementação RegisterController
    Modelagem inicial do controller responsável pelas rotas da aplicação.

--- server/src/main/java/br/com/server/controllers/RegisterController.java
+++ server/src/main/java/br/com/server/controllers/RegisterController.java
@@ -1,4 +1,40 @@
 package br.com.server.controllers;
 
+import br.com.server.entities.PessoaFisica;
+import br.com.server.entities.PessoaJuridica;
+import br.com.server.properties.PessoaFisicaRepository;
+import br.com.server.properties.PessoaJuridicaRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cadastro")
 public class RegisterController {
+
+    private final PessoaFisicaRepository pessoaFisicaRepository;
+    private final PessoaJuridicaRepository pessoaJuridicaRepository;
+
+    @Autowired
+    public RegisterController(PessoaFisicaRepository pessoaFisicaRepository, PessoaJuridicaRepository pessoaJuridicaRepository) {
+        this.pessoaFisicaRepository = pessoaFisicaRepository;
+        this.pessoaJuridicaRepository = pessoaJuridicaRepository;
+    }
+
+    @PostMapping("/pessoafisica")
+    public ResponseEntity<String> cadastrarPessoaFisica(@RequestBody @Valid PessoaFisica pessoaFisica) {
+        pessoaFisicaRepository.save(pessoaFisica);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Pessoa física cadastrada com sucesso!");
+    }
+
+    @PostMapping("/pessoajuridica")
+    public ResponseEntity<String> cadastrarPessoaJuridica(@RequestBody @Valid PessoaJuridica pessoaJuridica) {
+        pessoaJuridicaRepository.save(pessoaJuridica);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Pessoa jurídica cadastrada com sucesso!");
+    }
 }
